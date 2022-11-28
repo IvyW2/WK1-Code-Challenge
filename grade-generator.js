eda893478e1e12586def53bdd0d0a2038d52a046
IvyW2 <ivywambugha2@gmail.com>
2022-11-28 16:18:06 +0300

    Code challenge

---
+++ grade-generator.js
@@ -0,0 +1,19 @@
+//Challenge 1: Student Grade Generator
+
+ let score = prompt("Enter your score ") 
+
+ if (score <= 100 && score >= 80){
+    alert("You scored an A")
+}
+ else if (score<=60 && score >= 79){
+    alert("You scored a B")
+}
+ else if (score <=50 && score >= 59){
+    alert("You scored a C")
+}
+ else if (score <=40 && score >= 49){
+    alert("You scored a D")
+}
+ else if (score < 40 && score >= 0){
+     alert(" You scored an E")
+    }
